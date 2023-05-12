0333699fb24d67f00c0902f1ea335345f7e23536
itnextsolutions <you@example.com>
2023-05-12 11:36:56 +0530

    Sonali_Changes_12May

---
+++ Godrej_Korber_DAL/OrderINViewDL.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OracleClient;
+using Godrej_Korber_Shared.Models;
+
+namespace Godrej_Korber_DAL
+{
+	public  class OrderINViewDL
+	{
+		DataTable dt = new DataTable();
+		OracleHelper oracle = new OracleHelper();
+		SqlHelper objsqlHelper = new SqlHelper();
+		List<SqlParameter> SqlParameters = new List<SqlParameter>();
+		List<OracleParameter> OracleParameters = new List<OracleParameter>();
+
+		public DataTable GetOrder_IN_View()
+		{
+			DataTable dt = new DataTable();
+			OracleParameter[] param = new OracleParameter[1];
+			param[0] = new OracleParameter();
+			param[0].OracleType = OracleType.Cursor;
+			param[0].ParameterName = "orde_rcursor";
+			param[0].Direction = ParameterDirection.Output;
+
+			dt = oracle.ExecuteDataTable(oracle.GetConnection(), CommandType.StoredProcedure, "MRFWMS.ORDERVIEW.ORDVIEW", param);// packagename.spname
+			return dt;
+		}
+
+		public DataTable UpdateInOrderItm(HostToWmsModel wmsModel, int Hr)
+		{
+
+			OracleParameter[] param = new OracleParameter[3];
+
+			try
+			{
+				param[0] = new OracleParameter();
+				param[0].OracleType = OracleType.Int32;
+				param[0].ParameterName = "O_ORD_ID ";
+				param[0].Value = wmsModel.MSG_ORD_ID;
+				param[0].Direction = ParameterDirection.Input;
+
+				param[1] = new OracleParameter();
+				param[1].OracleType = OracleType.Int32;
+				param[1].ParameterName = "O_ORD_DT_REQUEST_Hr";
+				param[1].Value = Hr;
+				param[1].Direction = ParameterDirection.Input;
+
+				param[2] = new OracleParameter();
+				param[2].OracleType = OracleType.Cursor;
+				param[2].ParameterName = "OCCUR";
+				param[2].Direction = ParameterDirection.Output;
+
+				//dt=oracle.ExecuteDataTable(oracle.GetConnection(), CommandType.StoredProcedure, "MRFWMS.ORDERVIEW.UpdateOrdTimE", param);
+				dt = oracle.ExecuteDataTable(oracle.GetConnection(), CommandType.StoredProcedure, "MRFWMS.UPDATE_ORDER_TIME", param);
+				
+			}
+			catch(Exception ex)
+			{
+				throw ex;
+			}
+			return dt;
+		}
+	}	
+}
+
+
---
+++ Godrej_Korber_WebAPI/Controllers/OrderINViewController.cs
@@ -0,0 +1,74 @@
+﻿using Godrej_Korber_DAL;
+using Godrej_Korber_Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Godrej_Korber_WebAPI.Controllers
+{
+	//[Route("api/[controller]")]
+	//[ApiController]
+	public class OrderINViewController : ControllerBase
+	{
+
+		DataTable dt = new DataTable();
+		OrderINViewDL OrderINDal = new OrderINViewDL();
+
+		[Route("api/OrderINView/GetOrderdetail")]
+		[HttpGet]
+
+		public JsonResult GetOrderdetaildata()
+		{
+			dt = OrderINDal.GetOrder_IN_View();
+
+			List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+			Dictionary<string, object> childRow;
+			foreach (DataRow row in dt.Rows)
+			{
+				childRow = new Dictionary<string, object>();
+				foreach (DataColumn col in dt.Columns)
+				{
+					childRow.Add(col.ColumnName, row[col]);
+				}
+				parentRow.Add(childRow);
+			}
+			return new JsonResult(parentRow);
+		}
+	
+		[Route("api/OrderInnView/UpdateInOrderInView")]
+		[HttpPost]
+		public ActionResult UpdateInInOrderInView([FromBody] UpdateData wmsModels)
+		{
+			int Hrs = Convert.ToInt32(wmsModels.Hr);
+
+			foreach (var wmsModel in wmsModels.WmsModels)
+			{
+				//HostToWmsModel data = new HostToWmsModel
+
+				wmsModel.MSG_ORD_ID = wmsModel.MSG_ORD_ID;
+				wmsModel.MSG_ORD_DT_REQUEST = wmsModel.MSG_Hrs;
+
+				//set other properties of data object here
+
+				wmsModel.MSG_WRK_STN = System.Environment.MachineName;
+				wmsModel.MSG_WRK_USER = "SONALI KAMBLE";
+			
+
+				dt = OrderINDal.UpdateInOrderItm(wmsModel, Hrs);
+			 }
+
+			//foreach (HostToWmsModel data in wmsModels)
+			//{
+			//	int Hr = 4;
+			//	data.MSG_WRK_STN = System.Environment.MachineName;
+			//	data.MSG_WRK_USER = "SONALI KAMBLE";
+
+			//	dt = OrderINDal.UpdateInOrderItm(data, Hrs);
+			//}
+
+			return new JsonResult("Success");
+		}
+
+	}
+}
