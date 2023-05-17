2c79872416f07721f808a927b9c0f50f1efa19af
itnextsolutions <you@example.com>
2023-05-17 11:20:51 +0530

    Sonali-Empty-pallet

---
+++ Godrej_Korber_DAL/TataCummins/EmptyPalletDL.cs
@@ -0,0 +1,78 @@
+﻿
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.OracleClient;
+using Godrej_Korber_Shared.Models.TataCummins;
+using System;
+
+namespace Godrej_Korber_DAL.TataCummins
+{
+    public class EmptyPalletDL
+    {
+
+        DataTable dt = new DataTable();
+        OracleHelper oracle = new OracleHelper();
+        SqlHelper objsqlHelper = new SqlHelper();
+        List<SqlParameter> SqlParameters = new List<SqlParameter>();
+        List<OracleParameter> OracleParameters = new List<OracleParameter>();
+
+
+        public DataTable GetEmptyPalletOut(int palletnumber)
+        {
+           
+
+            OracleParameter[] param = new OracleParameter[2];
+
+            param[0] = new OracleParameter();
+            param[0].ParameterName = "PALLET_NUMBER";
+            param[0].OracleType = OracleType.Int32;
+            param[0].Value = palletnumber;
+            param[0].Direction = ParameterDirection.Input;
+
+            param[1] = new OracleParameter();
+            param[1].OracleType = OracleType.Cursor;
+            param[1].ParameterName = "OCUR";
+            param[1].Direction = ParameterDirection.Output;
+
+            dt = oracle.ExecuteDataTable(oracle.GetConnection(), CommandType.StoredProcedure, "MRFWMS.TATA_CUMMINS_EMPTY_PALLET.GET_EMPTY_PALLET_DATA", param);
+            return dt;
+        }
+
+        public DataTable InsertEmptyPallet(EmptyPallet emptypalletModel)
+        {
+
+            OracleParameter[] param = new OracleParameter[4];
+
+            param[0] = new OracleParameter();
+            param[0].ParameterName = "HU_ID";
+            param[0].OracleType = OracleType.Int32;
+            param[0].Value = emptypalletModel.HU_ID;
+            param[0].Direction= ParameterDirection.Input;
+
+			param[1] = new OracleParameter();
+			param[1].ParameterName = "HU_VOL";
+			param[1].OracleType = OracleType.Int32;
+			param[1].Value = emptypalletModel.HU_VOL;
+			param[1].Direction = ParameterDirection.Input;
+
+            param[2]= new OracleParameter();
+            param[2].ParameterName = "HU_HUPT_ID";
+            param[2].OracleType= OracleType.Float;
+            param[2].Value= emptypalletModel.HU_HUPT_ID;
+            param[2].Direction= ParameterDirection.Input;
+
+			param[3] = new OracleParameter();
+			param[3].OracleType = OracleType.Cursor;
+			param[3].ParameterName = "OCUR";
+			param[3].Direction = ParameterDirection.Output;
+
+			dt = oracle.ExecuteDataTable(oracle.GetConnection(), CommandType.StoredProcedure, "MRFWMS.TATA_CUMMINS_EMPTY_PALLET.INSERT_INTO_HUNIT1", param);
+			return dt;
+
+			//return dt;
+        }
+
+
+
+	}
+}
---
+++ Godrej_Korber_WebAPI/Controllers/Tata Cummins/EmptyPalletController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Godrej_Korber_DAL.TataCummins;
+using Godrej_Korber_Shared.Models.TataCummins;
+using System.Data;
+using Godrej_Korber_Shared.Models;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Godrej_Korber_WebAPI.Controllers.Tata_Cummins
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class EmptyPalletController : ControllerBase
+    {
+
+        DataTable dt = new DataTable();
+        EmptyPalletDL objEmptyPalletOut = new EmptyPalletDL();
+
+        // GET: api/<EmptyOutController>
+        [Route("api/EmptyPalletOut/GetEmptyPalletOut")]
+        [HttpGet]
+        public JsonResult GetEmptyPalletOut(EmptyPallet palletnumber)
+        {
+            dt = objEmptyPalletOut.GetEmptyPalletOut(palletnumber.PALLET_NUMBER);
+            List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+            Dictionary<string, object> childRow;
+            foreach (DataRow row in dt.Rows)
+            {
+                childRow = new Dictionary<string, object>();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    childRow.Add(col.ColumnName, row[col]);
+                }
+                parentRow.Add(childRow);
+            }
+            return new JsonResult(parentRow);
+        }
+
+		
+
+		[Route("api/EmptyPalletOut/InsertEmptyPalletData")]
+		[HttpPost]
+		public ActionResult InsertEmptyPalletData([FromBody] EmptyPallet wmsModel)
+		{
+
+			//wmsModel.MSG_WRK_STN = System.Environment.MachineName;
+			//wmsModel.MSG_WRK_USER = "SONALI KAMBLE";
+
+			dt = objEmptyPalletOut.InsertEmptyPallet(wmsModel);
+
+			int output = Convert.ToInt32(dt.Rows[0][0]);
+
+			if (output == 1)
+			{
+				return new JsonResult("Success");
+			}
+
+			else
+			{
+				return new JsonResult("Failed");
+			}
+		}
+
+
+		//update status from STK to RSV
+		
+	//	[Route("api/EmptyPallet/UpdateEmptyPallet")]
+	//	[HttpPost]
+	//	public ActionResult UpdateEmptyPallet([FromBody] EmptyPallet HU_ID)
+	//	{
+	//		foreach (var HU_ID in EmptyPallet.HU_ID)
+
+	//		{
+	//			//EmptyPallet.MSG_WRK_STN = System.Environment.MachineName;
+	//			//EmptyPallet.MSG_WRK_USER = "SONALI KAMBLE";
+
+	//			dt = objEmptyPalletOut.UpdateEmptyPalletcheck(HU_ID);
+	//		}
+	//		return new JsonResult(dt);
+
+	//}
+		                     
+	}
+}
