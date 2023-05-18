a70291ec801ba0e1ed4da6feeb9cba75ac4c58f7
itnextsolutions <itnextsolutions.web@gmail.com>
2023-05-18 13:43:51 +0530

    AJIT_18_MAY

---
+++ Godrej_Korber_DAL/TataCummins/StoreOutDL.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OracleClient;
+
+namespace Godrej_Korber_DAL.TataCummins
+{
+    public class StoreOutDL
+    {
+        OracleHelper objOracleHelper = new OracleHelper();
+        DataTable dtResult = new DataTable();
+
+        public DataTable Get_Store_Out_Data()
+        {
+            OracleParameter[] param = new OracleParameter[1];
+
+            param[0] = new OracleParameter();
+            param[0].OracleType = OracleType.Cursor;
+            param[0].ParameterName = "OCUR";
+            param[0].Direction = ParameterDirection.Output;
+
+            dtResult = objOracleHelper.ExecuteDataTable(objOracleHelper.GetConnection(),CommandType.StoredProcedure,"ORDERVIEW.GET_STORE_OUT_DATA",param);
+            return dtResult;
+
+        }
+    }
+}
---
+++ Godrej_Korber_WebAPI/Controllers/Tata Cummins/StoreOutController.cs
@@ -0,0 +1,36 @@
+﻿using Godrej_Korber_DAL.TataCummins;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Godrej_Korber_WebAPI.Controllers.Tata_Cummins
+{
+  
+    public class StoreOutController : ControllerBase
+    {
+        DataTable dtResult = new DataTable();
+        StoreOutDL objStoreOutDL = new StoreOutDL();
+
+
+        [Route("api/StoreOut/GetStoreOutData")]
+        [HttpGet]
+        public JsonResult Get_Store_Out_Data()
+        {
+            dtResult = objStoreOutDL.Get_Store_Out_Data();
+            List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+            Dictionary<string, object> childRow;
+            foreach (DataRow row in dtResult.Rows)
+            {
+                childRow = new Dictionary<string, object>();
+                foreach (DataColumn col in dtResult.Columns)
+                {
+                    childRow.Add(col.ColumnName, row[col]);
+                }
+                parentRow.Add(childRow);
+            }
+            return new JsonResult(parentRow);
+        }
+
+    }
+}
